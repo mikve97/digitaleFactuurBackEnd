5e8b0113f8c8d26dc92957f3e783d49ebcece428
ofahchouch <ofahchouch@yahoo.com>
2019-11-01 11:25:44 +0100

    created: migrations xml file

--- src/main/java/nl/dfbackend/git/MainConfiguration.java
+++ src/main/java/nl/dfbackend/git/MainConfiguration.java
@@ -5,15 +5,24 @@ import io.dropwizard.db.DataSourceFactory;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 public class MainConfiguration extends Configuration {
 	@NotEmpty
 	private String password;
+	@NotNull
+	@Valid
 	private DataSourceFactory dataSourceFactory = new DataSourceFactory();
 	
 	@JsonProperty
 	public String getPassword() {
 		return password;
+	}
+	
+	@JsonProperty("database")
+	public DataSourceFactory getDataSourceFactory() {
+		return dataSourceFactory;
 	}
 }
