d886c96acb570d610b33994b0643cb284afab118
ofahchouch <ofahchouch@yahoo.com>
2019-11-06 08:38:31 +0100

    created: tripMapper

---
+++ src/main/java/nl/dfbackend/git/mappers/TripMapper.java
@@ -0,0 +1,27 @@
+package nl.dfbackend.git.mappers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import nl.dfbackend.git.models.TripModel;
+
+public class TripMapper implements ResultSetMapper<TripModel> {
+
+	@Override
+	public TripModel map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+		return new TripModel(
+				r.getInt("id"), 
+				r.getInt("projectid"),
+				r.getInt("userid"),
+				r.getString("licenseplate"),
+				r.getString("startlocation"),
+				r.getString("endlocation"),
+				r.getDouble("startkilometergauge"),
+				r.getDouble("endkilometergauge")
+				);
+	}
+
+}
