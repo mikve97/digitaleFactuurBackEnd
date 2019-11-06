42fb1026f1bc189d8589a6ca6d311e6c870c6e59
ofahchouch <ofahchouch@yahoo.com>
2019-11-06 08:31:12 +0100

    created: initialize method in main and updated run resources

--- src/main/java/nl/dfbackend/git/MainApplication.java
+++ src/main/java/nl/dfbackend/git/MainApplication.java
@@ -1,7 +1,9 @@
 package nl.dfbackend.git;
 
 import io.dropwizard.Application;
-
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import nl.dfbackend.git.resources.ProjectResource;
 
@@ -13,12 +15,30 @@ public class MainApplication extends Application<MainConfiguration> {
         // new MainApplication().run(new String[] {"server", "config.yml"});
         new MainApplication().run(new String[] {"server"});
     }
+    
+    @Override
+    public String getName() {
+        return "digitaleFactuurBackEnd";
+    }
+    
+    @Override
+    public void initialize(final Bootstrap<MainConfiguration> bootstrap) {
+    	bootstrap.addBundle(new MigrationsBundle<MainConfiguration>() {
+            @Override
+                public DataSourceFactory getDataSourceFactory(MainConfiguration configuration) {
+                    return configuration.getDataSourceFactory();
+                }
+        });
+    }
 
     @Override
     public void run(MainConfiguration configuration, Environment environment) {
         // code to register module
         final ProjectResource resource = new ProjectResource();
-        environment.jersey().register(resource);
+        final TripResource tripResource = new TripResource();
+    	
+    	environment.jersey().register(resource);
+	    environment.jersey().register(tripResource);
     }
 
 }
