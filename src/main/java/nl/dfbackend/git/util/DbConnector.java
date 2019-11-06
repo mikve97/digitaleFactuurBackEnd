6441cbf558e4509af31a43250ee41445016becd3
ofahchouch <ofahchouch@yahoo.com>
2019-11-06 08:44:41 +0100

    created: dbConnector

--- src/main/java/nl/dfbackend/git/util/DbConnector.java
+++ src/main/java/nl/dfbackend/git/util/DbConnector.java
@@ -4,5 +4,30 @@ import org.postgresql.ds.PGPoolingDataSource;
 import org.skife.jdbi.v2.DBI;
 
 public class DbConnector {
+	private final static String url = "manny.db.elephantsql.com:5432";
+	private final static String dbName = "kvszgnmq";
+	private final static String username = "kvszgnmq";
+	private final static String password = "HLwhrVFmFDIO5LGAtLNPMghrHhQjEbdK";
+	private static PGPoolingDataSource source = null;
+	private static DbConnector singleInstance = null;
 	
+    public static DbConnector getInstance() 
+    { 
+        if (singleInstance == null) 
+        	singleInstance = new DbConnector();
+        	source  = new PGPoolingDataSource();
+        	getDBI();
+
+        return singleInstance; 
+    } 
+	
+	public static DBI getDBI() {
+		source.setServerName(url);
+		source.setDatabaseName(dbName);
+		source.setUser(username);
+		source.setPassword(password);
+		source.setMaxConnections(10);
+		
+		return new DBI(source);
+	}
 }
