6a11cb870e5d88b12dad64df4a6d6ef83020f638
mikeve97 <mikevanes97@gmail.com>
2019-11-08 02:04:44 +0100

    lat long added in DB changed trip files accordingly

--- src/main/java/nl/dfbackend/git/models/TripModel.java
+++ src/main/java/nl/dfbackend/git/models/TripModel.java
@@ -15,8 +15,13 @@ public class TripModel {
     private String endLocation;
     private double startKilometergauge;
     private double endKilometergauge;
+
+	private float startLat;
+	private float endLat;
+	private float startLong;
+	private float endLong;
     
-    public TripModel(int id, int projectId, int userId, String licensePlate, String startLocation, String endLocation, double startKilometergauge, double endKilometergauge) {
+    public TripModel(int id, int projectId, int userId, String licensePlate, String startLocation, String endLocation, double startKilometergauge, double endKilometergauge, float startLat, float endLat, float startLong, float endLong) {
 		this.tripId = id;
 		this.projectId = projectId;
 		this.userId = userId;
@@ -25,6 +30,10 @@ public class TripModel {
 		this.endLocation = endLocation;
 		this.startKilometergauge = startKilometergauge;
 		this.endKilometergauge = endKilometergauge;
+		this.startLat = startLat;
+		this.endLat = endLat;
+		this.startLong = startLong;
+		this.endLong = endLong;
 	}
 
 	public int getTripId() {
@@ -90,6 +99,18 @@ public class TripModel {
 	public void setEndKilometergauge(double endKilometergauge) {
 		this.endKilometergauge = endKilometergauge;
 	}
+
+	public void setStartLat(float startLat){this.startLat = startLat;}
+	public float getStartLat(){return this.startLat;}
+
+	public void setEndLat(float endLat){this.endLat = endLat;}
+	public float getEndLat(){return this.endLat;}
+
+	public void setStartLong(float startLong){this.startLong = startLong;}
+	public float getStartLong(){return this.startLong;}
+
+	public void setEndLong(float endLong){this.endLat = endLong;}
+	public float getEndLong(){return this.endLong;}
 	
     //equals, hashCode, getters and setters for all fields.
     @Override
