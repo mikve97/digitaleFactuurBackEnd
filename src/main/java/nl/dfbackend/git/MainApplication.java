d6a93168487a55a8a1bde468d14a6cfa33dc5653
ofahchouch <ofahchouch@yahoo.com>
2019-12-11 14:52:18 +0100

    created: CORS filter

--- src/main/java/nl/dfbackend/git/MainApplication.java
+++ src/main/java/nl/dfbackend/git/MainApplication.java
@@ -23,8 +23,12 @@ import org.jose4j.keys.HmacKey;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
 import java.sql.SQLException;
-
 import nl.dfbackend.git.resources.VehicleResource;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import java.util.EnumSet;
 
 
 public class MainApplication extends Application<MainConfiguration> {
@@ -60,11 +64,20 @@ public class MainApplication extends Application<MainConfiguration> {
 
     @Override
     public void run(MainConfiguration configuration, Environment environment) throws UnsupportedEncodingException, SQLException {
-        // code to register module
+    	final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+	    // Configure CORS parameters
+	    cors.setInitParameter("allowedOrigins", "*");
+	    cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+	    cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+	    // Add URL mapping
+	    cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+	    
         final ProjectResource resource = new ProjectResource(configuration.getApiKey(), configuration.getUserId());
         final TripResource tripResource = new TripResource();
         final VehicleResource vehicleResource = new VehicleResource();
-
+        
     	environment.jersey().register(resource);
 	    environment.jersey().register(tripResource);
 
